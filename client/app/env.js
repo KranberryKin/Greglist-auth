f2e453136a66dc3e8f5f211d0d5b9e61664ac578
Quinnton <quinnclimbs@gmail.com>
2021-09-14 10:35:10 -0600

    commit

---
+++ client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'berrykin.us.auth0.com'
+export const audience = 'https://gregslist-auth.com'
+export const clientId = 'qqFnGs3oO8O4DLqKTXyLfSRvCJ8cABBO'
